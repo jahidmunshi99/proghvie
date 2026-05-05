84530477471866a25f95b7998d2c60f258b11288
Jahid Munshi <jahidmunshi99@gmail.com>
2026-05-06 02:08:12 +0600

    update

--- app/components/forms/UserForm.js
+++ app/components/forms/UserForm.js
@@ -1,135 +1,212 @@
-import React from 'react'
+"use client";
 
-const UserForm = ({handleShow}) => {
-    const handleChange = ()=>{
+import { useEffect, useState } from "react";
+import {
+  addUser,
+  getDistrict,
+  getDivision,
+  getUpozila,
+} from "../../utils/db.js";
+import UserSubmitButton from "../UserSubmitButton.js";
 
-    }
+const UserForm = ({ handleShow }) => {
+  const [divisions, setDivisions] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  const [upozilas, setUpozilas] = useState([]);
+  const [addItem, setAddItem] = useState({
+    user_role: "",
+    user_name: "",
+    userId: "",
+    upazilaId: "",
+    phone: "",
+    emailId: "",
+    divisionId: "",
+    districtId: "",
+    created_at: new Date().toISOString(),
+  });
+
+  console.log(divisions);
+  useEffect(() => {
+    const fetchDivision = async () => {
+      const division_data = await getDivision();
+      const district_data = await getDistrict();
+      const upozila_data = await getUpozila();
+      setDivisions(division_data);
+      setDistricts(district_data);
+      setUpozilas(upozila_data);
+    };
+
+    fetchDivision();
+  }, []);
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setAddItem({
+      ...addItem,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addUser(addItem);
+    alert("You have sucessfully added your new user");
+  };
   return (
-
     <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
       <div className="bg-white w-full max-w-lg p-6 rounded-xl shadow-lg">
         <h2 className="text-lg font-semibold mb-4">Create new user</h2>
         <form className="space-y-4">
-
-        {/* <!-- UID --> */}
-        <div>
-            <label class="block text-sm font-medium text-gray-700 mb-1">User ID</label>
-            <input 
-            type="text" 
-            name="uid" 
-            placeholder="user_123"
-            class="w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+          {/* <!-- UID --> */}
+          <div>
+            <label class="block text-sm font-medium text-gray-700 mb-1">
+              User ID
+            </label>
+            <input
+              type="text"
+              name="userId"
+              value={addItem.userId}
+              onChange={handleChange}
+              placeholder="user_123"
+              class="w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
             />
-        </div>
+          </div>
 
-        {/* <!-- Name --> */}
-        <div>
-            <label class="block text-sm font-medium text-gray-700 mb-1">Name</label>
-            <input 
-            type="text" 
-            name="name" 
-            placeholder="Jahid Munshi"
-            class="w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+          {/* <!-- Name --> */}
+          <div>
+            <label class="block text-sm font-medium text-gray-700 mb-1">
+              Name
+            </label>
+            <input
+              type="text"
+              value={addItem.user_name}
+              onChange={handleChange}
+              placeholder="Jahid Munshi"
+              class="w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
             />
-        </div>
-        <div className='grid grid-cols-2 gap-3'>
+          </div>
+          <div className="grid grid-cols-2 gap-3">
             {/* <!-- email --> */}
             <div>
-                <label class="block text-sm font-medium text-gray-700 mb-1">Email</label>
-                <input 
-                type="email" 
-                name="email" 
+              <label class="block text-sm font-medium text-gray-700 mb-1">
+                Email
+              </label>
+              <input
+                type="email"
+                name="emailId"
+                value={addItem.emailId}
+                onChange={handleChange}
                 placeholder="example@example.com"
                 class="w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                />
+              />
             </div>
             {/* <!-- Phone --> */}
             <div>
-                <label class="block text-sm font-medium text-gray-700 mb-1">Phone Number</label>
-                <input 
-                type="text" 
-                name="phone" 
+              <label class="block text-sm font-medium text-gray-700 mb-1">
+                Phone Number
+              </label>
+              <input
+                type="text"
+                name="phone"
                 placeholder="01xxxxxxxxxx"
                 class="w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                />
+              />
             </div>
-        </div>
+          </div>
 
-        {/* <!-- Role --> */}
-        <div>
-            <label class="block text-sm font-medium text-gray-700 mb-1">Role</label>
-            <select 
-            name="role"
-            class="w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+          {/* <!-- Role --> */}
+          <div>
+            <label class="block text-sm font-medium text-gray-700 mb-1">
+              Role
+            </label>
+            <select
+              name="role"
+              class="w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
             >
-            <option value="division">Division Admin</option>
-            <option value="district">District Admin</option>
-            <option value="upazila" selected>Upazila Admin</option>
+              <option value="division">Division Admin</option>
+              <option value="district">District Admin</option>
+              <option value="upazila" selected>
+                Upazila Admin
+              </option>
             </select>
-        </div>
+          </div>
 
-
-        <div className='grid grid-cols-3 gap-3'>
+          <div className="grid grid-cols-3 gap-3">
             {/* <!-- Division --> */}
             <div>
-                <label class="block text-sm font-medium text-gray-700 mb-1">Division</label>
-                <select
+              <label class="block text-sm font-medium text-gray-700 mb-1">
+                Division
+              </label>
+              <select
                 name="divisionId"
                 onChange={handleChange}
-                className="w-full border px-4 py-2 rounded-lg"
+                className="w-full border px-4 py-2 rounded-lg capitalize"
                 required
-                >
+              >
                 <option value="">Select Division</option>
-                <option value="Rabi">Rabi</option>
-                <option value="Kharif">Kharif</option>
-                </select>
+                {divisions?.map((division) => (
+                  <option key={division.id} value={division.divisionId}>
+                    {division.divisionId}
+                  </option>
+                ))}
+              </select>
             </div>
 
             {/* <!-- District --> */}
             <div>
-                <label class="block text-sm font-medium text-gray-700 mb-1">District</label>
-                <select
-                name="divisionId"
+              <label class="block text-sm font-medium text-gray-700 mb-1">
+                District
+              </label>
+              <select
+                name="districtId"
                 onChange={handleChange}
-                className="w-full border px-4 py-2 rounded-lg"
+                className="w-full border px-4 py-2 rounded-lg capitalize"
                 required
-                >
+              >
                 <option value="">Select district</option>
-                <option value="Rabi">Rabi</option>
-                <option value="Kharif">Kharif</option>
-                </select>
+                {districts?.map((item) => (
+                  <option key={item.id} value={item.districtId}>
+                    {item.districtId}
+                  </option>
+                ))}
+              </select>
             </div>
             {/* <!-- Upazila --> */}
             <div>
-                <label class="block text-sm font-medium text-gray-700 mb-1">Upozila</label>
-                <select
+              <label class="block text-sm font-medium text-gray-700 mb-1">
+                Upozila
+              </label>
+              <select
                 name="upozilaId"
                 onChange={handleChange}
-                className="w-full border px-4 py-2 rounded-lg"
+                className="w-full border px-4 py-2 rounded-lg capitalize"
                 required
-                >
+              >
                 <option value="">Select Upozila</option>
-                <option value="Rabi">Rabi</option>
-                <option value="Kharif">Kharif</option>
-                </select>
+                {upozilas?.map((item) => (
+                  <option key={item.id} value={item.upozilaId}>
+                    {item.upozilaId}
+                  </option>
+                ))}
+              </select>
             </div>
-        </div>
+          </div>
 
-
-        {/* Buttons */}
+          {/* Buttons */}
           <div className="flex justify-end gap-3 pt-3">
-                    <button 
-            class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition" onClick={handleShow}
-        >Cancel</button>
-                <button 
-            type="submit" 
-            class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
-        >Submit</button>
+            <button
+              class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+              onClick={handleShow}
+            >
+              Cancel
+            </button>
+            <UserSubmitButton handleSubmit={handleSubmit} />
           </div>
         </form>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserForm
+export default UserForm;
--- app/utils/db.js
+++ app/utils/db.js
@@ -1,4 +1,10 @@
-import { addDoc, collection, getDocs, doc, deleteDoc  } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+} from "firebase/firestore";
 import { db } from "../../config/env-local.js";
 
 export const getFinancialYear = async () => {
@@ -11,20 +17,54 @@ export const getFinancialYear = async () => {
   }));
 };
 
-
 // Fetch Seedbed Collections information
 export const addSeedBedItem = async (data) => {
   const docRef = await addDoc(collection(db, "reports"), data);
   console.log("Document written with ID: ", docRef.id);
 };
 
-export const deleteItem = async (item)=>{
+export const deleteItem = async (item) => {
   try {
-      await deleteDoc(doc(db, "reports", item));
-      alert("You have successfully Deleted ites. Item Id:", item )
+    await deleteDoc(doc(db, "reports", item));
+    alert("You have successfully Deleted ites. Item Id:", item);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-    console.log("This is from firebase Delete Items", item.id)
+  console.log("This is from firebase Delete Items", item.id);
+};
 
-}
+export const getDivision = async () => {
+  const colRef = collection(db, "division_data");
+  const snapshot = await getDocs(colRef);
+
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
+
+export const getDistrict = async () => {
+  const colRef = collection(db, "district_data");
+  const snapshot = await getDocs(colRef);
+
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
+
+export const getUpozila = async () => {
+  const colRef = collection(db, "upozila_data");
+  const snapshot = await getDocs(colRef);
+
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
+
+// Add new User to DB
+export const addUser = async (user) => {
+  const docRef = await addDoc(collection(db, "users"), user);
+  console.log("Document written with ID: ", docRef.id);
+};
